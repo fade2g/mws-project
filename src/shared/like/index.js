f925944f562187ddd818911a3d4dccf19a453e49
Holger Heymanns <holger.heymanns@gmail.com>
2018-09-30 18:15:40 +0200

    Fix wrongly parsed is_favorite
    Don't take shortcuts...

--- src/shared/like/index.js
+++ src/shared/like/index.js
@@ -1,14 +1,14 @@
 export const likeBadge = (restaurant, container, toggleCallback) => {
   const heart = document.createElement("button");
   heart.type = "button";
+  heart.innerText = `like ${restaurant.name}`;
+  heart.classList.add("like");
+  if (restaurant.is_favorite === "true" || restaurant.is_favorite === true) {
+    heart.classList.add("favorite");
+  }
   heart.addEventListener("click", () => {
     heart.classList.toggle("favorite");
     toggleCallback(restaurant.id, heart.classList.contains("favorite"));
   });
-  heart.classList.add("like");
-  if (restaurant.is_favorite) {
-    heart.classList.add("favorite");
-  }
-  heart.innerText = "like";
   container.appendChild(heart);
 };
