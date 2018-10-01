9513859fd25899db46a6baba86b020b61b77650c
Holger Heymanns <holger.heymanns@gmail.com>
2018-10-01 18:26:53 +0200

    Add sanitization to form submission

--- src/reviewform/index.js
+++ src/reviewform/index.js
@@ -1,5 +1,6 @@
 import { reviewForm } from "./htmlhelper";
 import { postReview } from "../shared/api";
+import DOMPurify from "dompurify";
 
 /**
  * This function creates the reviewForm and handles the related events. Not that displaying and hoding the form is not part of this module.
@@ -18,10 +19,12 @@ export default class ReviewForm {
     this.createdNode = reviewForm(this.restaurantId);
     this.createdNode.addEventListener("submit", event => {
       postReview({
-        restaurant_id: parseInt(event.target.querySelector("#restaurant_id").value, 10),
-        name: event.target.querySelector("#reviewer_name").value,
-        rating: event.target.querySelector("#rating_options").value,
-        comments: event.target.querySelector("#review_comments").value
+        restaurant_id: parseInt(DOMPurify.sanitize(event.target.querySelector("#restaurant_id").value), 10),
+        name: DOMPurify.sanitize(event.target.querySelector("#reviewer_name").value),
+        rating: DOMPurify.sanitize(event.target.querySelector("#rating_options").value),
+        comments: DOMPurify.sanitize(event.target.querySelector("#review_comments").value)
+      }).then(response => {
+        console.log(response);
       });
       event.preventDefault();
       this.reviewFormContainer.classList.remove("form-visible");
